fad31ce73d9ec94b39e0f456b298cde9c1037ba5
ahmed <ahmed.dahdouh98@gmail.com>
2024-10-20 14:45:04 +0200

    backend app WIP

---
+++ app/models/models.py
@@ -0,0 +1,15 @@
+
+
+from sqlalchemy import Integer, String, Column, ForeignKey, Float,ARRAY
+
+from app.core.database import Base
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True,index=True)
+    username = Column(String(50), unique=True, nullable=False)
+    email = Column(String(50), unique=True, nullable=False)
+    password = Column(nullable=False)
+    vocal_data = Column(ARRAY(Float), nullable=True)
+
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI
+from app.core.database import engine
+from app.end_points.routes import router
+from app.models import models
 
 app = FastAPI()
+models.Base.metadata.create_all(bind=engine)
+app.include_router(router)
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("app.main:app", host="127.0.0.1", port=800, reload=True)
